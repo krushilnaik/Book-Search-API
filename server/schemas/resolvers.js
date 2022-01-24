85892bdcf09606e71f540d440e397979a667f48e
Krushil Naik <krushilnaik96@gmail.com>
2022-01-24 03:02:11 +0000

    Finish

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,77 +1,70 @@
-const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
-const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-	Query: {
-		me: async (_parent, _args, { user }) => {
-			if (user) {
-				const { _id } = user;
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
 
-				const userData = await User.findOne({ _id }).select("-password");
+        return userData;
+      }
 
-				return userData;
-			}
+      throw new AuthenticationError('Not logged in');
+    },
+  },
 
-			throw new AuthenticationError("Not logged in");
-		},
-	},
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-	Mutation: {
-		addUser: async (_parent, args) => {
-			const user = await User.create(args);
-			const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-			return { token, user };
-		},
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-		login: async (_parent, { email, password }) => {
-			const user = await User.findOne({ email });
+      const correctPw = await user.isCorrectPassword(password);
 
-			if (!user) {
-				throw new AuthenticationError("Incorrect credentials");
-			}
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-			const correctPw = await user.isCorrectPassword(password);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
 
-			if (!correctPw) {
-				throw new AuthenticationError("Incorrect credentials");
-			}
+        return updatedUser;
+      }
 
-			const token = signToken(user);
-			return { token, user };
-		},
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
 
-		saveBook: async (_parent, { bookData }, { user }) => {
-			if (user) {
-				const { _id } = user;
-				const updatedUser = await User.findByIdAndUpdate(
-					{ _id },
-					{ $push: { savedBooks: bookData } },
-					{ new: true }
-				);
+        return updatedUser;
+      }
 
-				return updatedUser;
-			}
-
-			throw new AuthenticationError("You need to be logged in!");
-		},
-
-		removeBook: async (_parent, { bookId }, { user }) => {
-			if (user) {
-				const { _id } = user;
-				const updatedUser = await User.findOneAndUpdate(
-					{ _id },
-					{ $pull: { savedBooks: { bookId } } },
-					{ new: true }
-				);
-
-				return updatedUser;
-			}
-
-			throw new AuthenticationError("You need to be logged in!");
-		},
-	},
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,47 +1,49 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-	type Query {
-		me: User
-	}
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(bookData: BookInput!): User
+  removeBook(bookId: ID!): User
+}
 
-	input BookInput {
-		authors: [String]
-		description: String!
-		bookId: String!
-		image: String
-		link: String
-		title: String!
-	}
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-	type Mutation {
-		login(email: String!, password: String!): Auth
-		addUser(username: String!, email: String!, password: String!): Auth
-		saveBook(bookData: BookInput!): User
-		removeBook(bookId: ID!): User
-	}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
 
-	type User {
-		_id: ID!
-		username: String!
-		email: String
-		bookCount: Int
-		savedBooks: [Book]
-	}
+input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
-	type Book {
-		bookId: ID!
-		authors: [String]
-		description: String
-		image: String
-		link: String
-		title: String!
-	}
+type Query {
+    me: User
+  }
 
-	type Auth {
-		token: ID!
-		user: User
-	}
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+
 `;
 
 module.exports = typeDefs;
