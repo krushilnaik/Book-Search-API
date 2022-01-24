0d88636bcd5b328f27d854b496d1daa1150ffceb
Krushil Naik <krushilnaik96@gmail.com>
2022-01-24 17:00:23 +0000

    Upload build directory

---
+++ client/build/precache-manifest.20178e90f33f4c225e0a2a0d9a9e4b9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72802b63b805b32d41abbbaf6bda2176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7d19c10bef772270cf0",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "eef17c9ca385d8632315",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "f7d19c10bef772270cf0",
+    "url": "/static/js/2.013104a5.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/static/js/2.013104a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef17c9ca385d8632315",
+    "url": "/static/js/main.9c18ce08.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
